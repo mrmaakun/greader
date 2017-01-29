8e5bf01b8cf15d891032ee713f105076b36ea5cf
Mark Serrano <mark.serrano@gmail.com>
2017-01-30 02:13:31 +0900

    deploy to heroku

--- content.go
+++ content.go
@@ -43,7 +43,7 @@ func downloadImage(imageId string) (string, error) {
 func saveAudio(audioData []byte) (string, error) {
 
 	// Save image file
-	audioFileName := "audio_" + strconv.Itoa(rand.Intn(10000)) + ".mp3"
+	audioFileName := "audio_" + strconv.Itoa(rand.Intn(10000)) + ".m4a"
 	newFile, err := os.Create("audio/" + audioFileName)
 
 	numBytesWritten, err := io.Copy(newFile, bytes.NewReader(audioData))
